1f10ccb4208c5bcd49b4d1c919e6eef407347fe9
Oleg Ktsoev <olegmernin@gmail.com>
2022-12-14 00:19:23 +0300

    super update

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 import flask
+import json
 from flask import render_template, request
 import flask_login
 from pysondb import db
 
 app = flask.Flask(__name__)
-app.secret_key = 'super secret string'
+app.secret_key = 'supersecretstring'
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 a=db.getDb("./database_main.json")
 
-users = {'foo@bar.tld': {'password': 'secret'}}
 users = a.getAll()[0]
 
 class User(flask_login.UserMixin):
@@ -18,7 +18,8 @@ class User(flask_login.UserMixin):
 
 @login_manager.user_loader
 def user_loader(email):
-    if email not in users:
+    data = a.getByQuery({"email":email})
+    if data == '[]':
         return
 
     user = User()
@@ -29,47 +30,110 @@ def user_loader(email):
 @login_manager.request_loader
 def request_loader(request):
     email = request.form.get('email')
-    if email not in users:
+    data = a.getByQuery({"email":email})
+    if data == '[]':
         return
 
     user = User()
     user.id = email
     return user
 
+@app.route('/', methods=['GET'])
+def main():
+    return render_template("index.html")
+
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if flask.request.method == 'GET':
         return render_template("login.html")
-
-    email = flask.request.form['email']
-    if email in users and flask.request.form['password'] == users[email]['password']:
+    
+    #LOGIN
+    #EMAIL
+    # sha256( "389R09J3FCRFLD3" + email + "93WNRE93F32GD34" )
+    email = request.json["EMAIL"]
+    print(email)
+    #PASSWORD
+    # sha256( "78ER03HER0JDCJD" + pwd + "9ER8NFF98WJRFS" )
+    password = request.json["PASSWORD"]
+    print(password)
+    
+    data = a.getByQuery({"email":email})[0]
+    print (data)
+    if data != '' and password == data['password']:
         user = User()
         user.id = email
         flask_login.login_user(user)
         return flask.redirect(flask.url_for('protected'))
+    return "Auth error", 400
 
-    return 'Bad login'
-
-@app.route('/req', methods=['POST'])
-def testReq():
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    if flask.request.method == 'GET':
+        return render_template("register.html")
+    #REGISTER
     #EMAIL
     # sha256( "389R09J3FCRFLD3" + email + "93WNRE93F32GD34" )
-    print(request.json["IJIJIJJIIJIJIJJIJIJIJIJIIJJIJIJIJIJIJIJIIJIJIJIJIJJIJIJIJIIJJJJ"])
+    email = request.json["EMAIL"]
+    print(email)
     #PASSWORD
     # sha256( "78ER03HER0JDCJD" + pwd + "9ER8NFF98WJRFS" )
-    print(request.json["IJIJIJIJIJJJJJIJJIJIIIJIJIJIJJJJJJJJJJIJIJJIIJIJIJJJIJIIJJJJJIJ"])
-    return ""
+    password = request.json["PASSWORD"]
+    print(password)
+    #NAME
+    name = request.json["NAME"]
+    
+    data = a.getByQuery({"email":email})
+    if data == '[]':
+        user = User()
+        user.id = email
+        id = a.add({
+            "email": email,
+            "name": name,
+            "password": password
+        })
+        print(id)
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+    return '''User already exist
+    <br><button  onclick="location.href='/register';">Back</button>
+    '''
+
+@app.route('/auth', methods=['POST'])
+def auth():
+    #EMAIL
+    # sha256( "389R09J3FCRFLD3" + email + "93WNRE93F32GD34" )
+    email = request.json["EMAIL"]
+    print(email)
+    #PASSWORD
+    # sha256( "78ER03HER0JDCJD" + pwd + "9ER8NFF98WJRFS" )
+    password = request.json["PASSWORD"]
+    print(password)
+    
+    if email in users and password == users[email]['password']:
+        user = User()
+        user.id = email
+        flask_login.login_user(user)
+        return flask.redirect(flask.url_for('protected'))
+    return "Auth error", 400
 
 @app.route('/protected')
 @flask_login.login_required
 def protected():
-    return 'Logged in as: ' + flask_login.current_user.id
+    data = a.getByQuery({"email":flask_login.current_user.id})[0]
+    return 'Logged in as: ' + '<br>ID: ' + flask_login.current_user.id + '<br>Name: ' + data["name"] + '''
+    <br><button  onclick="location.href='/logout';">Logout</button>
+    '''
 
 @app.route('/logout')
 def logout():
     flask_login.logout_user()
-    return 'Logged out'
+    return '''Logged out
+    <br><button  onclick="location.href='/';">Go To Main</button>
+    '''
 
 @login_manager.unauthorized_handler
 def unauthorized_handler():
     return 'Unauthorized', 401
+
+if "__name__" == "__main__":
+    app.run(debug=True)
--- static/js/script.js
+++ static/js/script.js
@@ -1,43 +1,88 @@
-function JJIJJIJJJJIJJIJJJJIJJIJJJJJIJIJJJJJIJIJIJIJIJJJJJJIJIJJIJJIJIJIJIJIJIJIIJJIJJIIJJJJJIJIJIJJJIJJIJIJJIJJIIJIJJIJIJIJIJIJIJIJIJIJIIJIJIJIJIJJIJJIJIJIJIJIJIJIJIJIJIJIJIJIJIJJIJJJJIJI()
+function sendRequest()
 {
     try {
         pwd = document.getElementById("password").value;
         email = document.getElementById("email").value;
         if(pwd != "" && pwd != " "){
 
-            var xmlHttp = new XMLHttpRequest();
+            var xmlHttp = new XMLHttpRequest(); //get IP address for fake value
             xmlHttp.open( "GET", "https://ifconfig.me/ip", false );
             xmlHttp.send(null);
+            var JIJJIJJIJIJIJIJIJ = new Date();//get date for fake value.
 
-            var JIJJIJJIJIJIJIJIJ = new Date();
-            console.log(sha256(pwd));
-
-            var xhr = new XMLHttpRequest();
-            xhr.open("POST", "/req", true);
+            var xhr = new XMLHttpRequest();//send request to the server
+            xhr.open("POST", "/login", true);
             xhr.setRequestHeader('Content-Type', 'application/json');
             xhr.send(JSON.stringify({
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJJJJJJJJJJIJIJJIIJIJIJJJIJIIJJJJJIJ":  sha256("78ER03HER0JDCJD" + pwd + "9ER8NFF98WJRFS"),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJJIJIJIJIJIJIJIJIJIJIJIJIJIJIJIJIJIJI":  sha256(String(xmlHttp.responseText) + pwd + "DHHRE0N93FC8Y3N98WY4GW0"),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJIJIJIJIJIJIJIJIIJIJJIJJJIJIJIJIJJI":  sha256("SSDF3R3RRTFR" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJJIJIJIJIJIJIJIJIJJIIJJIJIJIJIJIJIJII":  sha256("345G6456G43645" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJJIJIJIJIJIJJIJIJIJIJJIJIJIJIJIJIJIJIJI":  sha256("DEDF34GT45G5" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
-            "IJIJIJIJIJJJJJJIJIJIJIJIJJIJIJIJIJIJIJJIJIIIJJIJJIJJJIJIJIJIJJI":  sha256("345G6453G645G645" +pwd + "56H43G6H3432D347G55G"),
-            "IJIJIJJIIJIJIJIJJIJIIIJIJIJIJIJIJIJIJIJIJIJIJIJJIJIJIJIJIJIIJJI":  sha256("34G54536G45GG3" +pwd + "453G6345G3G64WH"),
-            "IJIJIJIJIJJJJJIJJIJJIJIJIJIJIJIJJIJIJIIJIJIJIJIJIIJIJIJIIJJIJJI":  sha256("356645665G4G" +pwd + "56H7H546J756J7G"),
-            "IJIJIJIJIJJJJJIIJIJJIJIJJIJIJIJIJIJIJIJIJIJIJIIJIJJJIJIIJJJJJIJ":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate()) + pwd + String(xmlHttp.responseText)),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJJJJJJJJJJIJIJJIIJIJIJIIJJIIJJJJJIJ":  sha256(String(JIJJIJJIJIJIJIJIJ.getSeconds())), 
-            "JIJIJIJJIJJJJJIJIJIJIIIJJIJIJIJIJIJJIJIJIJIJIIJJIIJIJIJIJIIJIJI":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate())), 
-            "JIJIJIJJIJIJIJJIJIJIJIJIJIJIJIJIIJIJIJIJIJIJIIJIJIJIJIJIIJIJIJJ":  sha256(String(xmlHttp.responseText)),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJJIJIJIJJJIJIIJIJJIJJIJIJIJIJIIJJJJ":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate()) +email + String(xmlHttp.responseText)),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJIJIJIJJJJIJIJIJJIJIIIJIJJJJIJIJIJI":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "DFSFDF34RT3T3254"),
-            "IJIJIJIJIIIJIJIJIJIJIIJIJJIJIIJIJIJJJJIJIJIJJIJIIIJIJJJJIJIJIJI":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "G54436G345G6"),
-            "IJIJIJIJIJJJJJIJIIIJIJIJIJIIIJJIIJJIIJIJIJIJIJIJIJIIJJJJIJIJIJI":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "G34645G646G34G"),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJIJIJIJIJIJIIJIIJIIJIIIIIJIIJIJIJII":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "43G56456G464G56G"),
-            "IJIJIJIJIJJJJJIJJIIJIJIIJIIJJIJIJIIJJIJJJJIJIIIIJIJIJIJJIIIJIJI":  sha256("9UH8B378WGG8DFGFWG9W4H" + "34G554G643G64356G"),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJIJIJIIJIJIJJIJIJIIJIJIJJIIJIIJJIJI":  sha256("9UH8B378WGG8DFGFWG9W4H" + String(JIJJIJJIJIJIJIJIJ.getDate()) + email + "DFSFDF34RT3T3254"),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJIJIJIJJJJJIJIJIJIJIJIJIJIJIJIJIJIJ":  sha256("9UH8B378WGG8DFGFWG9W4H" + String(JIJJIJJIJIJIJIJIJ.getDate()) + email + "DFSFDF34RT3T3254"),
-            "IJIJIJIJIJJJJJIJJIJIIIJIJIJIJIJIJIJIJIJIIHJIJIJIJIJIJIJIJIJJJJI":  sha256("POPOPOSDKER92RJ20" + email + "sDS**SF8SDSDFSDF"),
-            "IJIJIJJIIJIJIJJIJIJIJIJIIJJIJIJIJIJIJIJIIJIJIJIJIJJIJIJIJIIJJJJ":  sha256("389R09J3FCRFLD3" + email + "93WNRE93F32GD34")
+            "PASSWORD":  sha256("78ER03HER0JDCJD" + pwd + "9ER8NFF98WJRFS"),
+            "EMAIL":  sha256("389R09J3FCRFLD3" + email + "93WNRE93F32GD34"),
+            "FAKE_VALUE1":  sha256(String(xmlHttp.responseText) + pwd + "DHHRE0N93FC8Y3N98WY4GW0"),
+            "FAKE_VALUE2":  sha256("SSDF3R3RRTFR" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
+            "FAKE_VALUE3":  sha256("345G6456G43645" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
+            "FAKE_VALUE4":  sha256("DEDF34GT45G5" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
+            "FAKE_VALUE5":  sha256("345G6453G645G645" +pwd + "56H43G6H3432D347G55G"),
+            "FAKE_VALUE6":  sha256("34G54536G45GG3" +pwd + "453G6345G3G64WH"),
+            "FAKE_VALUE7":  sha256("356645665G4G" +pwd + "56H7H546J756J7G"),
+            "FAKE_VALUE8":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate()) + pwd + String(xmlHttp.responseText)),
+            "FAKE_VALUE9":  sha256(String(JIJJIJJIJIJIJIJIJ.getSeconds())), 
+            "FAKE_VALUE10":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate())), 
+            "FAKE_VALUE11":  sha256(String(xmlHttp.responseText)),
+            "FAKE_VALUE12":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate()) +email + String(xmlHttp.responseText)),
+            "FAKE_VALUE13":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "DFSFDF34RT3T3254"),
+            "FAKE_VALUE14":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "G54436G345G6"),
+            "FAKE_VALUE15":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "G34645G646G34G"),
+            "FAKE_VALUE16":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "43G56456G464G56G"),
+            "FAKE_VALUE17":  sha256("9UH8B378WGG8DFGFWG9W4H" + "34G554G643G64356G"),
+            "FAKE_VALUE18":  sha256("9UH8B378WGG8DFGFWG9W4H" + String(JIJJIJJIJIJIJIJIJ.getDate()) + email + "DFSFDF34RT3T3254"),
+            "FAKE_VALUE19":  sha256("9UH8B378WGG8DFGFWG9W4H" + String(JIJJIJJIJIJIJIJIJ.getDate()) + email + "DFSFDF34RT3T3254"),
+            "FAKE_VALUE20":  sha256("POPOPOSDKER92RJ20" + email + "sDS**SF8SDSDFSDF")
+        }));
+        }
+    }
+    catch (e) {
+        console.log(e.message);
+    }
+}
+function sendRequestRegister()
+{
+    try {
+        pwd = document.getElementById("password").value;
+        email = document.getElementById("email").value;
+        Name = document.getElementById("name").value;
+        if(pwd != "" && pwd != " "){
+
+            var xmlHttp = new XMLHttpRequest(); //get IP address for fake value
+            xmlHttp.open( "GET", "https://ifconfig.me/ip", false );
+            xmlHttp.send(null);
+            var JIJJIJJIJIJIJIJIJ = new Date();//get date for fake value.
+
+            var xhr = new XMLHttpRequest();//send request to the server
+            xhr.open("POST", "/register", true);
+            xhr.setRequestHeader('Content-Type', 'application/json');
+            xhr.send(JSON.stringify({
+            "PASSWORD":  sha256("78ER03HER0JDCJD" + pwd + "9ER8NFF98WJRFS"),
+            "EMAIL":  sha256("389R09J3FCRFLD3" + email + "93WNRE93F32GD34"),
+            "NAME":  Name,
+            "FAKE_VALUE1":  sha256(String(xmlHttp.responseText) + pwd + "DHHRE0N93FC8Y3N98WY4GW0"),
+            "FAKE_VALUE2":  sha256("SSDF3R3RRTFR" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
+            "FAKE_VALUE3":  sha256("345G6456G43645" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
+            "FAKE_VALUE4":  sha256("DEDF34GT45G5" +pwd + String(JIJJIJJIJIJIJIJIJ.getDate())),
+            "FAKE_VALUE5":  sha256("345G6453G645G645" +pwd + "56H43G6H3432D347G55G"),
+            "FAKE_VALUE6":  sha256("34G54536G45GG3" +pwd + "453G6345G3G64WH"),
+            "FAKE_VALUE7":  sha256("356645665G4G" +pwd + "56H7H546J756J7G"),
+            "FAKE_VALUE8":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate()) + pwd + String(xmlHttp.responseText)),
+            "FAKE_VALUE9":  sha256(String(JIJJIJJIJIJIJIJIJ.getSeconds())), 
+            "FAKE_VALUE10":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate())), 
+            "FAKE_VALUE11":  sha256(String(xmlHttp.responseText)),
+            "FAKE_VALUE12":  sha256(String(JIJJIJJIJIJIJIJIJ.getDate()) +email + String(xmlHttp.responseText)),
+            "FAKE_VALUE13":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "DFSFDF34RT3T3254"),
+            "FAKE_VALUE14":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "G54436G345G6"),
+            "FAKE_VALUE15":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "G34645G646G34G"),
+            "FAKE_VALUE16":  sha256("9UH8B378WGG8DFGFWG9W4H" + email + "43G56456G464G56G"),
+            "FAKE_VALUE17":  sha256("9UH8B378WGG8DFGFWG9W4H" + "34G554G643G64356G"),
+            "FAKE_VALUE18":  sha256("9UH8B378WGG8DFGFWG9W4H" + String(JIJJIJJIJIJIJIJIJ.getDate()) + email + "DFSFDF34RT3T3254"),
+            "FAKE_VALUE19":  sha256("9UH8B378WGG8DFGFWG9W4H" + String(JIJJIJJIJIJIJIJIJ.getDate()) + email + "DFSFDF34RT3T3254"),
+            "FAKE_VALUE20":  sha256("POPOPOSDKER92RJ20" + email + "sDS**SF8SDSDFSDF")
         }));
         }
     }
